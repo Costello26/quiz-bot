67955e4757e92075756e49457f6895a3e577a3db
Firdavs Ergashev <firdavsergashev@Firdavss-MBP.netis>
2022-06-21 11:40:54 +0500

    PM2 config initialized

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  apps : [{
+    name: 'oprosnik_bot',
+    script: 'app.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
